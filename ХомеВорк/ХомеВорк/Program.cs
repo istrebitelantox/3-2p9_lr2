2e156ed02969f0f48c792b5e49c87f887e00c721
istrebitelantox <91310724+istrebitelantox@users.noreply.github.com>
2021-10-06 10:14:51 +0300

    DZ

---
+++ ХомеВорк/ХомеВорк/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ХомеВорк
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drob d1 = new Drob(2,3);
+            Drob d2 = new Drob(2, 3, 4);
+            Drob d3 = new Drob(-3);
+            Console.WriteLine(d1.Des());
+            Console.WriteLine((d1 * d3).Des());
+            Console.WriteLine((d1 * d2).Znak);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ХомеВорк/ХомеВорк/Дроби.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ХомеВорк
+{
+    class Drob
+    {
+        int c;
+        int z;
+        public Drob(int ch, int zn)
+        {
+            c = ch;
+            z = zn;
+        }
+        public Drob(int chislichko)
+        {
+            c = chislichko;
+            z = 1;
+        }
+        public Drob(int celoe, int chi, int zna)
+        {
+            c = celoe * zna + chi;
+            z = zna;
+        }
+        public double Des()
+        {
+            return (double)(c) / z;
+        }
+        public static Drob operator *(Drob odin, Drob dva)
+        {
+            return new Drob(odin.c * dva.c, odin.z * dva.z);
+        }
+        public bool Znak
+        {
+            get
+            {
+                bool a = c >= 0;
+                bool b = z >= 0;
+                return a==b;
+            }
+        }
+        public delegate void ras(Drob saw, int znach);
+
+    }
+}
